385dafe9b8a157ae69e7d2e7207c8d7a5d71721c
Kevin Chung <kchung@nyu.edu>
2021-06-19 03:06:28 -0400

    Add more lint checks

--- ctfcli/utils/challenge.py
+++ ctfcli/utils/challenge.py
@@ -287,11 +287,42 @@ def lint_challenge(path):
     required_fields = ["name", "author", "category", "description", "value"]
     errors = []
     for field in required_fields:
-        if challenge.get(field) is None:
-            errors.append(field)
+        if field == "value" and challenge.get("type") == "dynamic":
+            pass
+        else:
+            if challenge.get(field) is None:
+                errors.append(field)
 
     if len(errors) > 0:
         print("Missing fields: ", ", ".join(errors))
+        exit(1)
+
+    # Check that the image field and Dockerfile match
+    if (Path(path).parent / "Dockerfile").is_file() and challenge.get("image") != ".":
+        print("Dockerfile exists but image field does not point to it")
+        exit(1)
+
+    # Check that Dockerfile exists and is EXPOSE'ing a port
+    if challenge.get("image") == ".":
+        try:
+            dockerfile = (Path(path).parent / "Dockerfile").open().read()
+        except FileNotFoundError:
+            print("Dockerfile specified in 'image' field but no Dockerfile found")
+            exit(1)
+
+        if "EXPOSE" not in dockerfile:
+            print("Dockerfile missing EXPOSE")
+            exit(1)
+
+    # Check that all files exists
+    files = challenge.get("files")
+    errored = False
+    for f in files:
+        fpath = Path(path).parent / f
+        if fpath.is_file() is False:
+            print(f"File {f} specified but not found at {fpath.absolute()}")
+            errored = True
+    if errored:
         exit(1)
 
     exit(0)
