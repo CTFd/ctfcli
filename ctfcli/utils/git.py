204cefb7a6dc08c9cd5560bd60ad32ffb2607d94
Kevin Chung <kchung@ctfd.io>
2022-03-01 15:26:48 -0500

    Improve ctf init to skip git repo creation and create directories

--- ctfcli/utils/git.py
+++ ctfcli/utils/git.py
@@ -11,3 +11,25 @@ def get_git_repo_head_branch(repo):
     ).decode()
     head_branch = out.split()[1]
     return head_branch
+
+
+def check_if_dir_is_inside_git_repo(dir=None):
+    """
+    Checks whether a given directory is inside of a git repo.
+    """
+    try:
+        out = (
+            subprocess.check_output(
+                ["git", "rev-parse", "--is-inside-work-tree"],
+                cwd=dir,
+                stderr=subprocess.DEVNULL,
+            )
+            .decode()
+            .strip()
+        )
+        print(out)
+        if out == "true":
+            return True
+        return False
+    except subprocess.CalledProcessError:
+        return False
