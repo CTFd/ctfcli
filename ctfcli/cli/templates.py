335d7b74b8b24924e42f5d96a9a68d9327e11ceb
Kevin Chung <kchung@ctfd.io>
2022-05-16 00:47:13 -0400

    Also print included templates

--- ctfcli/cli/templates.py
+++ ctfcli/cli/templates.py
@@ -2,7 +2,9 @@ import os
 import shutil
 import subprocess
 from glob import glob
+from pathlib import Path
 
+from ctfcli.utils.config import get_base_path
 from ctfcli.utils.templates import get_template_dir
 
 
@@ -18,6 +20,12 @@ class Templates(object):
         shutil.rmtree(template_dir)
 
     def list(self):
+        # Print included templates
+        path = Path(get_base_path()) / "templates"
+        for dir in path.iterdir():
+            print(dir.relative_to(path))
+
+        # Print installed templates
         template_dir = get_template_dir() + "/"
         for template in glob(f"{template_dir}/**/*/cookiecutter.json", recursive=True):
             # Remove prefix of template_dir and remove suffix of /cookiecutter.json
