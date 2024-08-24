01d44597c9b2e28848739a85179e5b2fa01771db
Kevin Chung <kchung@ctfd.io>
2024-08-24 04:06:05 -0400

    Require that local file exists on remote when use ctf media url

--- ctfcli/cli/media.py
+++ ctfcli/cli/media.py
@@ -59,6 +59,7 @@ class MediaCommand:
     def url(self, path):
         """Get server URL for a file key"""
         config = Config()
+        api = API()
 
         if config.config.has_section("media") is False:
             config.config.add_section("media")
@@ -66,9 +67,14 @@ class MediaCommand:
         try:
             location = config["media"][path]
         except KeyError:
-            click.secho(f"Could not locate media '{path}'", fg="red")
+            click.secho(f"Could not locate local media '{path}'", fg="red")
             return 1
 
-        base_url = config["config"]["url"]
-        base_url = base_url.rstrip("/")
-        return f"{base_url}{location}"
+        remote_files = api.get("/api/v1/files?type=page").json()["data"]
+        for remote_file in remote_files:
+            if f"/files/{remote_file['location']}" == location:
+                base_url = config["config"]["url"]
+                base_url = base_url.rstrip("/")
+                return f"{base_url}{location}"
+        click.secho(f"Could not locate remote media '{path}'", fg="red")
+        return 1
