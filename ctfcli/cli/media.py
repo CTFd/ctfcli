de36187f5172a7369654ebb88342c37ce48f0dd8
Kevin Chung <kchung@ctfd.io>
2024-08-24 04:04:25 -0400

    Add subcommand to get url for a media file

--- ctfcli/cli/media.py
+++ ctfcli/cli/media.py
@@ -1,5 +1,7 @@
 import os
 
+import click
+
 from ctfcli.core.api import API
 from ctfcli.core.config import Config
 
@@ -53,3 +55,20 @@ class MediaCommand:
                 del config["media"][path]
                 with open(config.config_path, "w+") as f:
                     config.write(f)
+
+    def url(self, path):
+        """Get server URL for a file key"""
+        config = Config()
+
+        if config.config.has_section("media") is False:
+            config.config.add_section("media")
+
+        try:
+            location = config["media"][path]
+        except KeyError:
+            click.secho(f"Could not locate media '{path}'", fg="red")
+            return 1
+
+        base_url = config["config"]["url"]
+        base_url = base_url.rstrip("/")
+        return f"{base_url}{location}"
